f0c1a33f3792e0e9f9722f793a42907009fc37ad
Wiz-97 <samuel97316@gmail.com>
2025-02-07 17:07:32 +0900

    Initial Commit

---
+++ TriviaQuestionAPI/TriviaQuestionAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaQuestionAPIApp.swift
+//  TriviaQuestionAPI
+//
+//  Created by Samuel Lee on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviaQuestionAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
